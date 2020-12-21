2e94d047169c77cb898d9840366ff840d74f0cf0
prabah <prabaharan.durairaj1@gmail.com>
2020-12-21 03:26:56 +0100

    test: added test for prob. coupling

---
+++ Probabilistic_coupling/tests/class_details.py
@@ -0,0 +1,9 @@
+def print_class_details():
+    print("classA { \n\t1 \n\t2 \n\t3 \n}")
+
+    print("testA{       testB{      testC{      testD{")
+    print("    1            1           2           3")
+    print("    2         }              3        }")
+    print("}                         }")
+    print("")
+
