e9702ea0d242c03beed551797bd4cd0affd88a52
prabah <prabaharan.durairaj1@gmail.com>
2021-01-11 14:22:11 +0100

    util: a utility to analyze new statement coverage result

--- line_coverage_analyser.py
+++ line_coverage_analyser.py
@@ -4,6 +4,11 @@ from util import read_config
 from utils import utils
 
 project_config = read_config(['project_details.properties'])
+project_name = "JacksonDatabind"
+defects4j_project_path = "/home/pepper/Dropbox/Documents/Study/Masters/thesis/projects/defects4j/framework/projects"
+current_project_path = defects4j_project_path + "/" + project_name
+file_path_new = current_project_path + "/trace_files/10f/" + "JacksonDatabind_10_checked-coverage.json"
+file_path = current_project_path + "/trace_files/10f/" + "checked_coverage.json"
 
 
 def list_diff(li1, li2):
@@ -12,31 +17,29 @@ def list_diff(li1, li2):
 
 
 def iterate_all_print():
-    project_name = "JacksonDatabind"
-    defects4j_project_path = "/home/pepper/Dropbox/Documents/Study/Masters/thesis/projects/defects4j/framework/projects"
-    current_project_path = defects4j_project_path + "/" + project_name
-
-    file_path = current_project_path + "/trace_files/10f/" + "line_coverage_new.json"
     statement_coverage = utils.read_json_file(file_path)
-    for classes in statement_coverage:
-        print("{} ->".format(classes))
-        # for tests in statement_coverage[classes]:
-        #     print("{} -> {}".format(tests, statement_coverage[classes][tests]))
+    # for classes in statement_coverage:
+    #     print("{} ->".format(classes))
+    #     # for tests in statement_coverage[classes]:
+    #     #     print("{} -> {}".format(tests, statement_coverage[classes][tests]))
+
+    print(len(utils.read_json_file(file_path)))
+    print(len(utils.read_json_file(file_path_new)))
+
+
+iterate_all_print()
 
 
 def print_given():
-    project_name = "JacksonDatabind"
-    defects4j_project_path = "/home/pepper/Dropbox/Documents/Study/Masters/thesis/projects/defects4j/framework/projects"
-    current_project_path = defects4j_project_path + "/" + project_name
-    given_class = "com.fasterxml.jackson.databind.AnnotationIntrospector"
-    given_test = "com.fasterxml.jackson.databind.TestFormatSchema::testFormatForGenerators"
-    file_path_new = current_project_path + "/trace_files/10f/" + "line_coverage_new.json"
-    file_path = current_project_path + "/trace_files/10f/" + "line_coverage.json"
+    given_class = "com.fasterxml.jackson.databind.ser.std.StdScalarSerializer"
+    given_test = "com.fasterxml.jackson.databind.creators.TestCreatorWithNamingStrategy556::testRenameViaFactory"
+
     statement_coverage = utils.read_json_file(file_path)
     statement_coverage_new = utils.read_json_file(file_path_new)
 
-    # for tests in statement_coverage_new[given_class]:
-    #     print(tests)
+    for tests in statement_coverage_new[given_class]:
+            print("{} -> {}".format(tests, statement_coverage_new[given_class][tests]))
+
     stop_at = 5
     counter = 0
     print_from = 5
@@ -46,23 +49,18 @@ def print_given():
             print("{} -> {}".format(classes, ""))
             for tests in statement_coverage[classes]:
                 try:
-                    statement_coverage_new[classes][tests]
-                    # print("{}{} -> {}".format("\t", tests, statement_coverage[classes][tests]))
-                    # print("{}{} -> {}".format("\t", tests, statement_coverage_new[classes][tests]))
+                    print("{}{} -> {}".format("\t", tests, statement_coverage[classes][tests]))
+                    print("{}{} -> {}".format("\t", tests, statement_coverage_new[classes][tests]))
                 except KeyError:
                     print(tests)
         if counter >= stop_at:
             break
 
 
+print_given()
+
+
 def class_diff_identifier():
-    project_name = "JacksonDatabind"
-    defects4j_project_path = "/home/pepper/Dropbox/Documents/Study/Masters/thesis/projects/defects4j/framework/projects"
-    current_project_path = defects4j_project_path + "/" + project_name
-    given_class = "com.fasterxml.jackson.databind.AnnotationIntrospector"
-    given_test = "com.fasterxml.jackson.databind.TestFormatSchema::testFormatForGenerators"
-    file_path_new = current_project_path + "/trace_files/10f/" + "line_coverage_new.json"
-    file_path = current_project_path + "/trace_files/10f/" + "line_coverage.json"
     statement_coverage = utils.read_json_file(file_path)
     statement_coverage_new = utils.read_json_file(file_path_new)
 
@@ -81,6 +79,6 @@ def class_diff_identifier():
     print(list_diff(statement_new_tests, statement_tests))
 
 
-class_diff_identifier()
+# class_diff_identifier()
 
 # com.fasterxml.jackson.databind.DeserializationConfig
